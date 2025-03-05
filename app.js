eb12d7b04c8ca202a0f38356ec52e7845df20ab3
Cristian Pinzon <cristian_0723@hotmail.com>
2025-03-04 22:19:37 -0500

    Se crea la funcion limpiar caja y agregar amigo

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let nombreamigo=document.getElementById("amigo");
+    if (nombreamigo == "" ){
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+    amigos.push(nombreamigo);
+    LimpiarCaja();
+}
+
+function LimpiarCaja(){
+    document.querySelector(`#valorusuario `);
+    valorCaja.value="";
+}
