b09d39980f22f3133627b0c72b5f231ab6a67355
Cristian Pinzon <cristian_0723@hotmail.com>
2025-03-07 18:40:49 -0500

    Agregamos la funcion sortear y actualizar

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-function agregarAmigo() {
-    let nombreamigo=document.getElementById("amigo");
+function agregarAmigo(){
+    let nombreamigo=document.getElementById("amigo").value;
     if (nombreamigo == "" ){
         alert("Por favor, inserte un nombre.");
         return;
     }
     amigos.push(nombreamigo);
+    Actualizarlista();
     LimpiarCaja();
+    
 }
 
 function LimpiarCaja(){
@@ -18,14 +20,23 @@ function LimpiarCaja(){
 
 function Actualizarlista(){
     let lista = document.getElementById("listaAmigos");
-    lista.innerHTML = "";//limpiamos la lista
-    for (let i = 0; i <amigos.length; i++) {
-        let li = document.createElement("li");
-        li.textContent=amigos[i]
+    lista.innerHTML = ""; // Limpiamos la lista antes de agregar los elementos
 
+    // Usando un bucle for para recorrer la lista de amigos
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li"); // Crear un elemento de lista
+        li.textContent = amigos[i]; // Asignar el nombre del amigo al texto del elemento
+        lista.appendChild(li); // Agregar el elemento de lista a la lista desordenada
     }
 }
 
-function sortearamigos(){
-    
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("La lista de amigos está vacía.");
+        return;
+    }
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSeleccionado = amigos[indiceAleatorio];
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>Amigo seleccionado: ${amigoSeleccionado}</li>`;
 }
